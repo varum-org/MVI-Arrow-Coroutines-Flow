0d421f26a1cf977af7ef9f256303523a0b78eea1
Nguyen Ngoc Duy <nguyenngocduy@Nguyens-MacBook-Pro.local>
2021-03-20 08:25:12 +0700

    [Fix][Splash] Goood bye SingleEventLiveData :v

--- app/src/main/java/com/vtnd/duynn/presentation/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/vtnd/duynn/presentation/ui/splash/SplashViewModel.kt
@@ -6,19 +6,22 @@ import androidx.lifecycle.viewModelScope
 import com.vtnd.duynn.domain.usecase.CheckAuthUseCase
 import com.vtnd.duynn.utils.extension.Event
 import com.vtnd.duynn.utils.extension.asLiveData
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 /**
  * Created by duynn100198 on 3/19/21.
  */
 class SplashViewModel(private val checkAuthUseCase: CheckAuthUseCase) : ViewModel() {
-    private val _authEvent = MutableLiveData<Event<Boolean>>()
-    val authEvent get() = _authEvent.asLiveData()
+    private val _authEvent = MutableSharedFlow<Boolean>()
+    val authEvent get() = _authEvent.asSharedFlow()
 
     init {
         viewModelScope.launch {
             val auth = checkAuthUseCase.invoke().fold(ifLeft = { false }, ifRight = { it })
-            _authEvent.value = Event(auth)
+            _authEvent.emit(auth)
         }
     }
 }
